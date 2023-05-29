2e5b5d42d2483425b2067559da2262c383a00bd6
Kevin McAleer <kevinmcaleer@gmail.com>
2023-05-29 19:11:06 +0100

    updated code

--- pico_a.py
+++ pico_a.py
@@ -6,7 +6,8 @@ serial.init(9600, bits=8, parity=None, stop=1)
 
 count = 0
 while True:
-    if serial.any():
-        print(serial.read())
-    serial.write(str(count).encode('UTF-8'))
+    if serial.any() > 0:
+        print(serial.read().decode('utf-8'))
+    serial.write(bytes(count, 'utf-8'))
     count += 1
+
--- pico_b.py
+++ pico_b.py
@@ -6,7 +6,7 @@ serial.init(9600, bits=8, parity=None, stop=1)
 
 count = 0
 while True:
-    if serial.any():
-        print(serial.read())
-    serial.write(str(count).encode('UTF-8'))
+    if serial.any() > 0:
+        print(serial.read().decode('utf-8'))
+    serial.write(bytes(str(count), 'utf-8'))
     count += 1
