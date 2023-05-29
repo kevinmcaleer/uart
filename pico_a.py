d54ca8a434e4c3092942dce632c39deeb36ed40a
Kevin McAleer <kevinmcaleer@gmail.com>
2023-05-29 19:36:21 +0100

    Update pico_a.py

--- pico_a.py
+++ pico_a.py
@@ -1,14 +1,15 @@
 from machine import UART
 
-serial = UART(0)
+serial = UART(0, 9600, bits=8, parity=None, stop=1)
 
-serial.init(9600, bits=8, parity=None, stop=1)
+serial.init()
 
 count = 0
 while True:
     if serial.any() > 0:
         message = serial.read()
-        print(message.decode("utf-8"))
+        decoded_message = bytes(c for c in message if c < 128).decode("utf-8")
+        print(decoded_message)
     serial.write(bytes(str(count), "utf-8"))
     count += 1
 
